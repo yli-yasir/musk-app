c70780e6dc2305b56f4e97622be948e0a998c30d
yli-yasir <35302828+yli-yasir@users.noreply.github.com>
2022-03-03 23:40:39 +0000

    upgrade to laravel 9

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,8 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::get('/greeting', function () {
-    return 'Hello Wrld';
 });
